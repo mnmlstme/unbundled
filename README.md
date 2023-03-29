a1c253743b7ce0008c8fa160dc0b9aab995bf6db
Ken Kubiak <ken@mnmlst.me>
2023-03-28 18:20:33 -0700

    ready for gh-pages

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# Web Development, Unbundled 
+# Web Development, Unbundled
 
 These projects demonstrate modern framework-free web development, based on Web Standards
+
+This site is generated with [Eleventy](https://www.11ty.dev),
+using the [kram-11ty] plugin to generate live
+previews from code examples.
--- eleventy.config.js
+++ eleventy.config.js
@@ -24,57 +24,11 @@ module.exports = function (eleventyConfig) {
   // Override Markdown parser to Kram
   eleventyConfig.addExtension("md", kram11ty.configure());
 
-  // Transform for running our Kram output through WebC
-  false &&
-    eleventyConfig.addTransform("webc-html", async function (content) {
-      const { WebC } = await import("@11ty/webc");
-
-      if (
-        this.page.inputPath.endsWith(".md") &&
-        this.page.outputPath &&
-        this.page.outputPath.endsWith(".html")
-      ) {
-        console.log("Transforming WebC to HTML:", this.page.outputPath);
-
-        const page = new WebC();
-        const outputDir = path.dirname(this.page.outputPath);
-
-        page.defineComponents("src/components/cre-ative/*.webc");
-        page.setBundlerMode(true);
-        page.setContent(content);
-
-        const { html, css, js, svg, components } = await page.compile();
-        let dependencies = [];
-
-        await fs.mkdir(outputDir, { recursive: true });
-
-        if (css) {
-          const cssOutputPath = path.join(outputDir, "styles.css");
-          console.log(`Writing ${cssOutputPath}`);
-          await fs.writeFile(cssOutputPath, css.join("\n"));
-          dependencies.push(cssOutputPath);
-        }
-
-        if (js) {
-          const jsOutputPath = path.join(outputDir, "script.js");
-          console.log(`Writing ${jsOutputPath}`);
-          await fs.writeFile(jsOutputPath, js.join("\n"));
-          dependencies.push(jsOutputPath);
-        }
-
-        // this.addDependencies(this.page.inputPath, dependencies);
-
-        return html;
-      }
-
-      return content;
-    });
-
   // Return your Object options:
   return {
     dir: {
       input: "src/chapters",
-      output: "site",
+      output: "docs",
     },
   };
 };
